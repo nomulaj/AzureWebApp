1d1d2b0dd585d25fa95c05c8a4656b0fba3178d2
nomulaj <46794132+nomulaj@users.noreply.github.com>
2019-01-17 14:10:25 -0500

    Add files via upload

---
+++ AzureJavaWebapp-CICDwithMaven/src/MyHttpServlet.java
@@ -0,0 +1,32 @@
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyHttpServlet
+ */
+@WebServlet("/MyHttpServlet")
+public class MyHttpServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyHttpServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("My Servlet Version");
+		response.getWriter().print("Output from Java class");
+	}
+
+}
